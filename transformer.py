58df32c808c7eb03045b5a43977a1990fa21328c
Wenlong Chen <60941044+chenw20@users.noreply.github.com>
2024-01-26 00:23:12 +0800

    Update transformer.py

--- transformer.py
+++ transformer.py
@@ -1,10 +1,10 @@
+import numpy as np
 import numpy.random as npr
 import torch
-
 import torch.nn as nn
 from mlp import Embeddings, ClassficationHead, FC
-from sgp_layer_trans import SGP_LAYER
-
+from sgp_layer import SGP_LAYER
+                
 
 class Transformer(torch.nn.Module):
     def __init__(self, device, vocab_size, depth, max_len, embdim, num_class, hdim, num_heads, sample_size, jitter, drop_rate, keys_len, kernel_type, flag_sgp):
@@ -25,7 +25,7 @@ class Transformer(torch.nn.Module):
         self.embdim = embdim
         self.vocab_size = vocab_size
 
-        self.embedding = Embeddings(vocab_size=vocab_size,max_len=max_len,emb_size=embdim ,h_size=hdim,drop_rate=drop_rate)
+        self.embedding = Embeddings(vocab_size=vocab_size,max_len=max_len,emb_size=embdim,h_size=hdim,drop_rate=drop_rate)
         
         self.class_head = ClassficationHead(hdim=hdim, num_class=num_class, drop_rate=drop_rate)
 
@@ -33,7 +33,7 @@ class Transformer(torch.nn.Module):
 
         self.ln = nn.LayerNorm(hdim)
 
-        self.keys = nn.ParameterList([nn.Parameter(torch.tensor(npr.randn(self.num_heads,1,self.keys_len,self.embdim+1024), dtype=torch.float32))]+[nn.Parameter(torch.tensor(npr.randn(self.num_heads, 1, self.keys_len, self.hdim), dtype=torch.float32)) for i in range(1,self.depth)])
+        self.keys = nn.ParameterList([nn.Parameter(torch.tensor(npr.randn(self.num_heads, 1, self.keys_len, self.hdim), dtype=torch.float32)) for i in range(self.depth)])
 
         self.sgp_layer_list = nn.ModuleList([SGP_LAYER(device=device, num_heads=num_heads, max_len=max_len, hdim=hdim, kernel_type=self.kernel_type, drop_rate=self.drop_rate, keys_len=self.keys_len, sample_size=self.sample_size, jitter=jitter, flag_sgp=flag_sgp)])
         self.mlp_layer_list = nn.ModuleList([FC(hdim=hdim, drop_rate=self.drop_rate)])
@@ -42,21 +42,21 @@ class Transformer(torch.nn.Module):
             self.sgp_layer_list.append(SGP_LAYER(device=device, num_heads=num_heads, max_len=max_len, hdim=hdim, kernel_type=self.kernel_type, drop_rate=self.drop_rate, keys_len=self.keys_len, sample_size=1, jitter=jitter, flag_sgp=flag_sgp))
             self.mlp_layer_list.append(FC(hdim=hdim, drop_rate=self.drop_rate))
 
-    def forward(self, input_data,positional,input_mask, data):
-        emb_ln, emb, keys0 = self.embedding.forward(input_data,positional,self.keys[0], self.device, data) 
+    def forward(self, input_data,positional,input_mask):
+        emb_ln, emb = self.embedding.forward(input_data,positional) 
         
-        z, total_kl = self.sgp_layer_list[0].forward(emb_ln, keys0, input_mask) 
+        z, total_kl = self.sgp_layer_list[0].forward(emb_ln, self.keys[0], input_mask) 
         z_prime = emb.unsqueeze(1) + z 
         z_ln = self.ln(z_prime) 
         
         z = self.mlp_layer_list[0].forward(z_ln) + z_prime 
-        if self.depth > 1:
+        if self.depth>1:
             cur_k = self.mlp_layer_list[0].forward(self.keys[1]) + self.keys[1] 
         for i in range(1, self.depth):
             z_prev = z.reshape(-1, z.shape[-2], z.shape[-1]) 
             z_ln = self.ln(z_prev)  
-            cur_k = self.ln(cur_k)
-            z, kl = self.sgp_layer_list[i].forward(z_ln, cur_k, input_mask)
+            cur_k = self.ln(cur_k) 
+            z, kl = self.sgp_layer_list[i].forward(z_ln, cur_k, input_mask) 
             if total_kl:
                 total_kl += kl
             z_prime = z_prev.unsqueeze(1) + z  
@@ -67,35 +67,65 @@ class Transformer(torch.nn.Module):
         logits = self.class_head.forward(z, input_mask).squeeze(1) 
         return logits, total_kl 
     
-    def loss(self, input_data,answers,positional,input_mask, data, anneal_kl=1.):
-        logits, total_kl = self.forward(input_data,positional,input_mask, data) 
+    def loss(self, input_data,answers,positional,input_mask, anneal_kl=1.):
+        logits, total_kl = self.forward(input_data,positional,input_mask) 
         ce_loss = nn.CrossEntropyLoss()
-        answers = answers.unsqueeze(1) 
-        answers = answers.unsqueeze(1).tile(1, self.sample_size, 1).view(-1, answers.shape[1]) 
+        answers = torch.unsqueeze(answers,1) 
+        answers = torch.tile(torch.unsqueeze(answers, 1), (1, self.sample_size, 1)).view(-1, answers.shape[1]) 
         neg_ElogPyGf = ce_loss(logits.view(-1, self.num_class), answers.view(-1))
-        if total_kl:
+        if total_kl and total_kl.item() > 0:
             loss = neg_ElogPyGf + anneal_kl* total_kl
         else:
             loss = neg_ElogPyGf
         return loss
     
+    def acc_nll(self, dev_loader):
+        scalar=0
+        nll_loss = nn.NLLLoss()
+        acc = 0
+        dev_size = 0
+        for _, data in enumerate(dev_loader, 0):
+            input_data = data['ids'].to(self.device, dtype = torch.long)
+            input_mask = data['mask'].to(self.device, dtype = torch.long)
+            answers = data['targets'].to(self.device, dtype = torch.long)
+            dev_size += len(answers)
+            
+            batch_max_len = torch.max(torch.sum(input_data != 1, 1)).item()
+            input_data = input_data[:, :batch_max_len]
+            input_mask = input_mask[:, :batch_max_len]
+        
+            positional = torch.tile(torch.tensor(np.arange(batch_max_len), dtype=torch.long).unsqueeze(0), (len(answers) ,1)).to(self.device)
+            
+            if self.sample_size == 1 and self.flag_sgp: 
+                logits = torch.stack([self.forward(input_data,positional,input_mask)[0] for _ in range(10)])
+                pred_probs = torch.mean(torch.softmax(logits, -1), 0) 
+            else:
+                logits, _ = self.forward(input_data,positional,input_mask) 
+                logits = logits.reshape(-1, self.sample_size, self.num_class) 
+                pred_probs = torch.mean(torch.softmax(logits, -1), 1) 
+            _, pred_hard = torch.max(pred_probs, -1) 
+            scalar += nll_loss(torch.log(pred_probs).view(-1, self.num_class), answers.view(-1)).item() * len(answers)
+            acc += torch.sum(pred_hard.view(-1) == answers.view(-1)).item()
+        return acc/ dev_size, scalar/ dev_size
+
     def pred_nll(self, data_loader):
         nll_sum = 0
         answers_list = []
         pred_hard_list = []
         nll_loss = nn.NLLLoss()
+    
         for i in range(data_loader.num_batches):
-            data,input_data,input_mask, positional,answers=data_loader.__load_next__()
+            _,input_data,input_mask, positional,answers=data_loader.__load_next__()
             if self.sample_size == 1 and self.flag_sgp: 
-                logits = torch.stack([self.forward(input_data,positional,input_mask, data)[0] for _ in range(10)])
+                logits = torch.stack([self.forward(input_data,positional,input_mask)[0] for _ in range(5)])
                 pred_probs = torch.mean(torch.softmax(logits, -1), 0) 
             else:
-                logits, _ = self.forward(input_data,positional,input_mask, data) 
-                logits = logits.reshape(-1, self.sample_size, self.num_class)
+                logits, _ = self.forward(input_data,positional,input_mask) 
+                logits = logits.reshape(-1, self.sample_size, self.num_class) 
                 pred_probs = torch.mean(torch.softmax(logits, -1), 1) 
         
             _, pred_hard = torch.max(pred_probs, -1) 
-            answers = answers.unsqueeze(1) #
+            answers = torch.unsqueeze(answers,1) 
             pred_hard_list.append(pred_hard)
             answers_list.append(answers)
 
@@ -103,6 +133,7 @@ class Transformer(torch.nn.Module):
         
         pred_hard_total = torch.cat(pred_hard_list)
         answers_total = torch.cat(answers_list).squeeze(-1)
+        
         tn = torch.sum((answers_total == 0) * (pred_hard_total == 0)).item()
         tp = torch.sum((answers_total == 1) * (pred_hard_total == 1)).item()
         fp = torch.sum((answers_total == 0) * (pred_hard_total == 1)).item()
